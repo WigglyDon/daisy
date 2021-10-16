2c916eddbac65fb6b8f0c806882d4f6f1fad58a1
Donnie Walsh <donfather1000@gmail.com>
2021-10-16 03:51:42 +0000

    added test container to try and fill with boilerplate DB

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-
-  $("button").on("click", function (event) {
+  $(".test_button").on("click", function () {
     const daisy_text = "h1"
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     $(daisy_text).css("color", `#${randomColor}`);
+  });
 
+  $(".database_button").on("click", function () {
+    $(".listing_container").append("this is a test");
   });
 
 });
