94bcb41b55cf310f5388f2965a8ccc74a0812e7c
Sarah <sarahejohnson89@gmail.com>
2021-10-16 12:39:40 -0700

    add 02_plants.sql and 03_listings.sql tables

---
+++ db/schema/02_plants.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE plants (
+  id SERIAL PRIMARY KEY NOT NULL,
+  picture_url VARCHAR(255) NOT NULL,
+)
---
+++ db/schema/03_listings.sql
@@ -0,0 +1,6 @@
+CREATE TABLE listings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  plant_name VARCHAR(255),
+  price INTEGER,
+  quantity INTEGER
+)
