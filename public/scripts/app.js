4b731a17a22ec1348e44af39bf811f84cd4d7d64
Donnie Walsh <donfather1000@gmail.com>
2021-10-16 03:37:09 +0000

    make button which randomly changes the color of DAISY via ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
-// Client facing scripts here
+$(document).ready(function () {
+
+  $("button").on("click", function (event) {
+    const daisy_text = "h1"
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    $(daisy_text).css("color", `#${randomColor}`);
+
+  });
+
+});
