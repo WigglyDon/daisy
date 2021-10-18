a9562438eb3cf9aef7fa85181690bab8953c22c8
Donnie Walsh <donfather1000@gmail.com>
2021-10-18 07:26:05 +0000

    updated listings template

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
-// Note: Feel free tasdasddsdso replace the example routes below with your own
+// Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const listingsRoutes = require("./routes/listings");
 
