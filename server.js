45c18bbf39fe79d21a929c528940eadc1f492b6a
Sarah <sarahejohnson89@gmail.com>
2021-10-19 14:57:13 -0700

    refactored search syntax and all listing show on home and admin page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cookieSession = require("cookie-session");
 
 app.use(cookieSession({
 
-  name: 'COOKIE session',
+  name: 'COOKIE',
   // keys: ['test'],
   signed: false,
   maxAge: 24 * 60 * 60 * 100
@@ -55,6 +55,8 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const listingsRoutes = require("./routes/listings");
+const { request } = require("express");
+const { reset } = require("nodemon");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -66,19 +68,57 @@ app.use("/users", usersRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
+//HOME
 app.get("/", (req, res) => {
-  res.render("index");
+  if (req.session.user) {
+    res.render("index");
+  } else {
+    res.render("admin");
+  }
 });
+
+
+//LOGIN
+app.get("/login", (req, res) => {
+  req.session = {
+    user: 'John'
+  };
+
+  res.redirect("/")
+});
+
+///LOGOUT
+app.post("/logout", (req, res) => {
+
+  req.session = null;
+  res.send('endpoint for /logout method POST')
+});
+
+//LISTINGS
+app.post("/listings", (req, res) => {
+  res.send('endpoint for /listings method POST')
+});
+
 
 
 app.get("/listings", (req, res) => {
   const searchQuery = req.query.search;
+  const limit = Number(req.query.limit);
+  console.log("searchQuery", searchQuery);
   let query = `
     SELECT name, picture_url
     FROM listings
-    JOIN plants on plant_id = plants.id
-    WHERE name LIKE '%${searchQuery}%';
-    `;
+    JOIN plants on plant_id = plants.id`;
+
+  if (searchQuery && searchQuery.length)
+    query += ` WHERE name LIKE '%${searchQuery}%'`;
+  console.log("limit", limit);
+
+  if (limit > 0)
+    query += ` LIMIT ${limit}`;
+
+  console.log('query = ', query);
+
   db.query(query)
     .then(data => {
       const listings = data.rows
@@ -94,23 +134,8 @@ app.get("/listings", (req, res) => {
 });
 
 
-app.get("/login", (req, res) => {
 
-  req.session = {
-    user: 'John'
-  };
-  res.send(`endpoint for /listings/:id method GET`);
-});
 
-app.post("/listings", (req, res) => {
-  res.send('endpoint for /listings method POST')
-});
-
-app.post("/logout", (req, res) => {
-
-  req.session = null;
-  res.send('endpoint for /logout method POST')
-});
 
 
 app.listen(PORT, () => {
