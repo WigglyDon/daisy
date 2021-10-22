c9076b92040497e9b783d59e3734f7ba0fcc0d27
Sarah <sarahejohnson89@gmail.com>
2021-10-22 09:15:30 -0700

    added more seeds to database in separate file

---
+++ db/seeds/06_more_seeds_sk.sql
@@ -0,0 +1,26 @@
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Orchids', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ4UTCTIcfQkyeRdkBgOEo3-0mTu46G8Wv6g&usqp=CAU', 11, 5, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Hydrangeas', 'https://www.gardeningknowhow.com/wp-content/uploads/2016/06/potted-hydrangea.jpg', 10, 4, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Red Roses', 'https://www.witherspoonrose.com/product_images/uploaded_images/potted.jpg', 9, 3, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ( 'Purple Pansies', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVw4gz2OQKvTA8tql9lQ0Y7QbP2oF1A_9WWA&usqp=CAU', 8, 15, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Pink Dahlias', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F07%2F23%2Fpink-dahlias-growing-in-container.jpg', 6, 10, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ( 'Red Bell Peppers', 'https://www.ballseed.com/dispthumb.aspx?imgsize=Display&imageid=211000', 5, 9, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Early Flame Jalapeno Hot Peppers', 'https://edge.bonnieplants.com/www/tiny/uploads/20200810203711/Early-Flame_5250.jpg', 2, 10, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Yellow Bell Peppers', 'https://gardenerspath.com/wp-content/uploads/2019/03/Grow-Bell-Peppers-in-the-Home-Garden.jpg',10, 30, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ( 'Cherry Tomatoes', 'https://plantinstructions.com/wp-content/uploads/2017/05/cherry-tomatoes-in-pot.jpg', 11, 15, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Tropical Plan Mixes', 'https://i.pinimg.com/736x/1b/79/58/1b795851c839fad96d8e1cec67a7b048--one-pot-tropical-plants.jpg', 25, 50, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Yellow Honeysuckles', 'https://img.alicdn.com/i3/valley-of-flowers-wan-four-seasons-honeysuckle-honeysuckle-seedlings-potted-seedlings-four-years-long-flowering-leaves-and-flowers-can-make-tea/TB1ik4nGFXXXXavXpXXXXXXXXXX_!!2-item_pic.png', 2,10, FALSE);
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Pear Trees', 'https://i.ebayimg.com/images/g/JKEAAOSwLyVZz98u/s-l400.jpg', 11, 4, FALSE);
+
+
+INSERT INTO listings (name, picture_url, price, quantity, favorited) VALUES ('Wild Strawberry Plant', 'https://cdn.shopify.com/s/files/1/0156/0137/products/Wild_Strawberry_plant_1200x960_a2f198b5-c952-4e69-98ff-f01210e0e219.jpg', 5, 60, FALSE);
